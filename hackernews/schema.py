51e7491c35234570bb93ddbe43907ea4d6d60317
Felipe Pontes <felipemfpontes@gmail.com>
2018-01-19 02:04:48 -0300

    :sparkles: Add Relay support

--- hackernews/schema.py
+++ hackernews/schema.py
@@ -1,14 +1,25 @@
 import graphene
 
 import links.schema
+import links.schema_relay
 import users.schema
 
 
-class Query(users.schema.Query, links.schema.Query, graphene.ObjectType):
+class Query(
+    users.schema.Query,
+    links.schema_relay.RelayQuery, 
+    links.schema.Query, 
+    graphene.ObjectType
+):
     pass
 
 
-class Mutation(users.schema.Mutation, links.schema.Mutation, graphene.ObjectType):
+class Mutation(
+    users.schema.Mutation,
+    links.schema_relay.RelayMutation,
+    links.schema.Mutation,
+    graphene.ObjectType
+):
     pass
 
 
