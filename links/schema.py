72ff130ec8d19fb70d12f4ae7fd9246fb723cbc1
Felipe Pontes <felipemfpontes@gmail.com>
2018-01-19 00:32:12 -0300

    :sparkles: Add createLink mutation

--- links/schema.py
+++ links/schema.py
@@ -9,8 +9,32 @@ class LinkType(DjangoObjectType):
         model = Link
 
 
+class CreateLink(graphene.Mutation):
+    id = graphene.Int()
+    url = graphene.String()
+    description = graphene.String()
+
+    class Arguments:
+        url = graphene.String()
+        description = graphene.String()
+
+    def mutate(self, info, url, description):
+        link = Link(url=url, description=description)
+        link.save()
+
+        return CreateLink(
+            id=link.id,
+            url=link.url,
+            description=link.description,
+        )
+
+
 class Query(graphene.ObjectType):
     links = graphene.List(LinkType)
 
     def resolve_links(self, info, **kwargs):
         return Link.objects.all()
+
+
+class Mutation(graphene.ObjectType):
+    create_link = CreateLink.Field()
