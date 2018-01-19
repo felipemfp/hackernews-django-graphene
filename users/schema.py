0449098d504d784304daa5f5ceaeca245213b568
Felipe Pontes <felipemfpontes@gmail.com>
2018-01-19 01:25:31 -0300

    :sparkles: Add a bit of authentication

--- users/schema.py
+++ users/schema.py
@@ -1,7 +1,21 @@
 import graphene
 from graphene_django import DjangoObjectType
+from django.contrib.auth import authenticate
 
 from users.models import User
+
+
+def get_user(info):
+    token = info.context.session.get('token')
+
+    if not token:
+        return
+
+    try:
+        user = User.objects.get(token=token)
+        return user
+    except:
+        raise Exception('User not found!')
 
 
 class UserType(DjangoObjectType):
@@ -28,12 +42,38 @@ class CreateUser(graphene.Mutation):
         return CreateUser(user=user)
 
 
+class LogIn(graphene.Mutation):
+    user = graphene.Field(UserType)
+
+    class Arguments:
+        username = graphene.String()
+        password = graphene.String()
+
+    def mutate(self, info, username, password):
+        user = authenticate(username=username, password=password)
+
+        if not user:
+            raise Exception('Invalid username or password!')
+
+        info.context.session['token'] = user.token
+        return LogIn(user=user)
+
+
 class Mutation(graphene.ObjectType):
     create_user = CreateUser.Field()
+    login = LogIn.Field()
 
 
 class Query(graphene.ObjectType):
+    me = graphene.Field(UserType)
     users = graphene.List(UserType)
 
     def resolve_users(self, info):
         return User.objects.all()
+
+    def resolve_me(self, info):
+        user = get_user(info)
+        if not user:
+            raise Exception('Not authenticated!')
+
+        return user
