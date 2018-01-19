1de2c597d1651cbeb1a9e9a47245c0f8cf7f5cb5
Felipe Pontes <felipemfpontes@gmail.com>
2018-01-19 01:46:20 -0300

    :sparkles: Add search ability

--- links/schema.py
+++ links/schema.py
@@ -1,8 +1,15 @@
 import graphene
 from graphene_django import DjangoObjectType
+from graphql import GraphQLError
+from django.db.models import Q
 
-from links.models import Link
+from links.models import Link, Vote
 from users.schema import get_user, UserType
+
+
+class VoteType(DjangoObjectType):
+    class Meta:
+        model = Vote
 
 
 class LinkType(DjangoObjectType):
@@ -38,12 +45,47 @@ class CreateLink(graphene.Mutation):
         )
 
 
-class Query(graphene.ObjectType):
-    links = graphene.List(LinkType)
+class CreateVote(graphene.Mutation):
+    user = graphene.Field(UserType)
+    link = graphene.Field(LinkType)
 
-    def resolve_links(self, info, **kwargs):
+    class Arguments:
+        link_id = graphene.Int()
+
+    def mutate(self, info, link_id):
+        user = get_user(info) or None
+        if not user:
+            raise GraphQLError('You must be logged to vote!')
+
+        link = Link.objects.filter(id=link_id).first()
+        if not link:
+            raise Exception('Invalid Link!')
+
+        Vote.objects.create(
+            user=user,
+            link=link,
+        )
+
+        return CreateVote(user=user, link=link)
+
+
+class Query(graphene.ObjectType):
+    links = graphene.List(LinkType, search=graphene.String())
+    votes = graphene.List(VoteType)
+
+    def resolve_links(self, info, search=None, **kwargs):
+        if search:
+            filter = (
+                Q(url__icontains=search) |
+                Q(description__icontains=search)
+            )
+            return Link.objects.filter(filter)
         return Link.objects.all()
+
+    def resolve_votes(self, info, **kwargs):
+        return Vote.objects.all()
 
 
 class Mutation(graphene.ObjectType):
     create_link = CreateLink.Field()
+    create_vote = CreateVote.Field()
