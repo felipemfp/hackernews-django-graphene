62afb0f89cdd4fa08cc70b25fcdb0ed125739b5a
Felipe Pontes <felipemfpontes@gmail.com>
2018-01-19 01:30:31 -0300

    :sparkles: Add relation between posts and user

--- links/models.py
+++ links/models.py
@@ -4,3 +4,4 @@ from django.db import models
 class Link(models.Model):
     url = models.URLField()
     description = models.TextField(null=True, blank=True)
+    posted_by = models.ForeignKey('users.User', null=True, on_delete=models.deletion.CASCADE)
--- links/schema.py
+++ links/schema.py
@@ -2,6 +2,7 @@ import graphene
 from graphene_django import DjangoObjectType
 
 from links.models import Link
+from users.schema import get_user, UserType
 
 
 class LinkType(DjangoObjectType):
@@ -13,19 +14,27 @@ class CreateLink(graphene.Mutation):
     id = graphene.Int()
     url = graphene.String()
     description = graphene.String()
+    posted_by = graphene.Field(UserType)
 
     class Arguments:
         url = graphene.String()
         description = graphene.String()
 
     def mutate(self, info, url, description):
-        link = Link(url=url, description=description)
+        user = get_user(info) or None
+
+        link = Link(
+            url=url,
+            description=description,
+            posted_by=user,
+        )
         link.save()
 
         return CreateLink(
             id=link.id,
             url=link.url,
             description=link.description,
+            posted_by=link.posted_by
         )
 
 
